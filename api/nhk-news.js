ab48cfbe72b075638d3523f3c21b10ad8b070a38
junichill <ilovehawks777@gmail.com>
2025-12-29 12:10:58 +0900

    Create nhk-news.js

---
+++ api/nhk-news.js
@@ -0,0 +1,13 @@
+// api/nhk-news.js
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://www3.nhk.or.jp/news/easy/top-list.json");
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "NHKニュース取得失敗" });
+  }
+}
