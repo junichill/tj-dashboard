5bf9e64ff63910f00a17d91112b717cdc0b58306
junichill <ilovehawks777@gmail.com>
2025-12-29 22:07:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ function initClock(tick) {
     setInterval(function () {
         const now = new Date();
 
-        // 時刻を HHMMSS の文字列にする
+        // 時刻を HH:MM:SS の文字列にする
         const hours = String(now.getHours()).padStart(2, '0');
         const minutes = String(now.getMinutes()).padStart(2, '0');
         const seconds = String(now.getSeconds()).padStart(2, '0');
-        const str = hours + minutes + seconds;
+
+        // コロンを入れる
+        const str = `${hours}:${minutes}:${seconds}`;
 
         // tick に値をセット
         tick.value = str;
 
         // aria-label に時間表示を入れる（アクセシビリティ対応）
-        tick.root.setAttribute('aria-label', `${hours}:${minutes}:${seconds}`);
+        tick.root.setAttribute('aria-label', str);
     }, 1000);
 
 }
