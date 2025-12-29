c6982ee3a5bb32f650071019a1bc9d9423e31ff8
junichill <ilovehawks777@gmail.com>
2025-12-29 12:16:50 +0900

    Update nhk-news.js

--- api/nhk-news.js
+++ api/nhk-news.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
-    console.error(err);
     res.status(500).json({ error: "NHKニュース取得失敗" });
   }
 }
